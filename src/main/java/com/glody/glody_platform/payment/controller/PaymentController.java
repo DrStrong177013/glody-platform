2fcfcc09492ed045bed17e0da2adf4ffa5653fac
DrStrong177013 <manhtdse173612@fpt.edu.vn>
2025-06-17 14:11:28 +0700

    test05 return VNPay

--- src/main/java/com/glody/glody_platform/payment/controller/PaymentController.java
+++ src/main/java/com/glody/glody_platform/payment/controller/PaymentController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +21,7 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class PaymentController {
