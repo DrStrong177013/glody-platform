ab7ceb25074e7aad7f83bd310aac495a2a638d4b
DrStrong177013 <149897720+DrStrong177013@users.noreply.github.com>
2025-08-03 19:00:08 +0700

    .

---
+++ src/main/java/com/glody/glody_platform/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.glody.glody_platform.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .exposedHeaders("*")
+                        .maxAge(86400);
+            }
+        };
+    }
+}
