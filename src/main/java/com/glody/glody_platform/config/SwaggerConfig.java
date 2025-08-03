ac34a67e92f42c261e0050873da7e2d9b0164971
DrStrong177013 <149897720+DrStrong177013@users.noreply.github.com>
2025-08-03 19:58:42 +0700

    swagger config

--- src/main/java/com/glody/glody_platform/config/SwaggerConfig.java
+++ src/main/java/com/glody/glody_platform/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.glody.glody_platform.config;
 
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
@@ -7,6 +8,8 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,6 +21,7 @@ public class SwaggerConfig {
                         .title("Glody Study Abroad API")
                         .description("API documentation for Glody Platform")
                         .version("1.0.0"))
+                .servers(List.of(new Server().url("https://pog.threemusketeer.click")))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
